a303e403ac75c1ac23ede52294b8e886519eaf33
FacuGerez <facundogerezuba@hotmail.com>
2024-04-30 16:51:44 -0300

    Finished descomposeLU function

--- algorithms/SE/descomposeLU.py
+++ algorithms/SE/descomposeLU.py
@@ -2,7 +2,7 @@ import numpy as np
 from gaussjordansolution.gauss import gauss
 from typing import Tuple
 
-def descompose(A: np.ndarray, b: np.array, mantize: int = 8) -> Tuple[np.ndarray, np.ndarray]:
+def descompose(A: np.ndarray, b: np.array, mantize: int = 8) -> Tuple[np.ndarray, np.ndarray,np.array]:
     if len(A) == 0 or len(A) != len(A[0]) or len(A) != len(b):
         raise Exception("The matrix and vector do not have the same size")
     if A.dtype != float:
@@ -12,7 +12,26 @@ def descompose(A: np.ndarray, b: np.array, mantize: int = 8) -> Tuple[np.ndarray
 
     new_a = np.column_stack((A, b))
     (gauss_result,changes) = gauss(new_a, len(new_a[0]), len(new_a), mantize,LU=True)
-    print(gauss_result)
+    L = np.zeros_like(A)
+    U = np.zeros_like(A)
+    for fil in range(len(A)):
+        L[fil][fil] = 1
+        for col in range(fil):
+            L[fil][col] = gauss_result[fil][col]
+        for col in range(fil, len(A)):
+            U[fil][col] = gauss_result[fil][col]
+
+    """ P is not necessary for the descomposition of LU
+
+    Perm = np.zeros_like(A)
+    for i in range(len(changes)):
+        Perm[i][changes[i]] = 1
+    
+    """
+
+    return L,U,changes
+
+
 
 
 descompose(np.array([[7,2,3],[7,5,4],[7,8,9]]),np.array([1,2,8]))
