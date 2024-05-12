6e7deecda77d14971dcf02cb6f184affce6eb0ca
FacuGerez <facundogerezuba@hotmail.com>
2024-05-12 11:38:53 -0300

    fix errors ENL

--- algorithms/ENL/newton_raphson.py
+++ algorithms/ENL/newton_raphson.py
@@ -1,7 +1,7 @@
-def newton_raphson(x: float, f: function[float], fderiv: function[float], errormin: float) -> float | None:
+def newton_raphson(x: float, f, fderiv, errormin: float) -> float | None:
     xold: float = x
     xnew: float = xold - (f(xold) / fderiv(xold))
-    while xold - xnew > errormin:
+    while abs(xold - xnew) > errormin:
         xold: float = xnew
         xnew: float = xold - (f(xold) / fderiv(xold))
     return xnew
--- algorithms/ENL/secante.py
+++ algorithms/ENL/secante.py
@@ -1,11 +1,11 @@
-def secante(x: float, f: function[float], errormin: float) -> float | None:
+def secante(x: float, f, errormin: float) -> float | None:
     if f(x) == 0:
         return x
     xold: float = x
     xmed: float = xold - min((f(xold) / ((f(xold) - f(xold - 1)) / (xold - (xold - 1)))),
                              (f(xold) / ((f(xold) - f(xold + 1)) / (xold - (xold + 1)))))
     xnew: float = xmed - (f(xmed) / ((f(xmed) - f(xold)) / (xmed - xold)))
-    while max(xold, xnew) - min(xold, xnew) > errormin:
+    while abs(xold - xnew) > errormin:
         if f(xmed) == 0:
             return xmed
         if f(xnew) == 0:
