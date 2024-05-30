5d89a220fdc4dc6a5f75b359de64f5a2e73b5170
FacuGerez <facundogerezuba@hotmail.com>
2024-05-30 17:11:37 -0300

    Tp result in b

--- tp.py
+++ tp.py
@@ -20,18 +20,30 @@ def main():
     """
 
     #-----------------------A2--------------------------------
+    def modelar(fV,fC,tk,h):
+        i = 0
+        NewV = 0
+        NewC = cons["C0"]
+        print("i={0}/// t={1} /// V={2} /// C={3}".format(i,0,NewV,NewC))
+        while NewV >= 0 and i >= 0: #corta la iteracion cuando NewV es menor a 0 osea q se vacio
+            t = h*i # Aca se calcula el tiempo desde t=0 hasta t= 0+h*i donde h avanza de a 1 minuto
+            NewV = _euler_Exp(NewV, t, lambda _,y: fV(NewC,tk,t,y), h) # Aca se hace el euler paso a paso de la ecuacion 1
+            NewC = _euler_Exp(NewC, t, lambda _,y: fC(NewV,y), h) # Aca se hace el euler paso a paso de la ecuacion 6
+            i+=1
+            print("i={0} /// t={1} /// V={2} /// C={3}".format(i,t+h,NewV,NewC))
+
     tk = tiempo[3] # [5min,10min,15min,30min,1h,3h,6h,12h,24h,72h]
     h = 1/60
-    i = 0
-    NewV = 0
-    NewC = cons["C0"]
-    print("i={0}/// t={1} /// V={2} /// C={3}".format(i,0,NewV,NewC))
-    while NewV >= 0 and i >= 0: #corta la iteracion cuando NewV es menor a 0 osea q se vacio
-        t = h*i # Aca se calcula el tiempo desde t=0 hasta t= 0+h*i donde h avanza de a 1 minuto
-        NewV = _euler_Exp(NewV, t, lambda _,y: (Qent(NewC,tk)- Qsal(y)) if t<tk else (0 - Qsal(y)), h) # Aca se hace el euler paso a paso de la ecuacion 1
-        NewC = _euler_Exp(NewC, t, lambda _,y: C(NewV,y), h) # Aca se hace el euler paso a paso de la ecuacion 6
-        i+=1
-        print("i={0} /// t={1} /// V={2} /// C={3}".format(i,t+h,NewV,NewC))
+    modelar(lambda C,tk,t,newV: (Qent(C,tk)- Qsal(newV)) if t<tk else (0 - Qsal(newV)),lambda V,newC: C(V,newC),tk,h)
+    #modelar(fV                                                                       ,fC                      ,tk,h)
 
     #-----------------------B1--------------------------------
+    """tk = tiempo[0] # [5min,10min,15min,30min,1h,3h,6h,12h,24h,72h]
+    h = 1/60
+    NewQmax = 15.0463125 #aproximadamente
+    BQsal = lambda V: NewQmax * math.sqrt((cons["Hmax"] - (cons["Hs"]-(V/cons["Asot"])))/(cons["Hmax"]-cons["Hmin"]))
+    modelar(lambda C,tk,t,newV: (Qent(C,tk)- BQsal(newV)) if t<tk else (0 - BQsal(newV)),lambda V,newC: C(V,newC),tk,h)
+    #modelar(fV                                                                         ,fC                      ,tk,h)"""
+
+
 main()
