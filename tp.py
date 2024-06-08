66f621148ebf0b0767e5774360afede84d9e048b
FacuGerez <facundogerezuba@hotmail.com>
2024-06-08 13:20:35 -0300

    Refactor code to calculate h<0.25

--- tp.py
+++ tp.py
@@ -26,13 +26,13 @@ def main():
         i = 0
         NewV = 0
         NewC = cons["C0"]
-        resultado.append([0, NewV, NewC]) # Aca se guarda el primer resultado en la lista
+        resultado.append([0, NewV, NewC, NewV/cons["Asot"]]) # Aca se guarda el primer resultado en la lista
         while NewV >= 0 and i >= 0: #corta la iteracion cuando NewV es menor a 0 osea q se vacio
             t = h*i # Aca se calcula el tiempo desde t=0 hasta t= 0+h*i donde h avanza de a 1 minuto
             NewV = model(NewV, t, lambda _,y: fV(NewC,t,y), h) # Aca se hace el euler paso a paso de la ecuacion 1
             NewC = model(NewC, t, lambda _,y: fC(NewV,y), h) # Aca se hace el euler paso a paso de la ecuacion 6
             i+=1
-            resultado.append([t+h, NewV, NewC]) # Aca se guarda el resultado de cada iteracion en la lista
+            resultado.append([t+h, NewV, NewC,NewV/cons["Asot"]]) # Aca se guarda el resultado de cada iteracion en la lista
         return resultado
 
     with open("tablas.txt", "w") as archivo: # Abre el archivo en modo escritura
@@ -55,18 +55,24 @@ def main():
                             h,
                             _euler_Exp)
             archivo.write(f"\n\nA2) with tk = {tk}h and h= {h} \n")
-            tabulado = tabulate(resultA2,headers=["Tiempo", "Volumen", "C"],tablefmt='grid',stralign='center', numalign= 'center')
+            tabulado = tabulate(resultA2,headers=["Tiempo", "Volumen", "C", "H"],tablefmt='grid',stralign='center', numalign= 'center')
             archivo.write(tabulado)
             """print()
             print("A2 with tk = ",tk,"h and h= ",h)
             print(tabulado)
             _ = input("Press Enter to continue...")"""
 
+        #-----------------------B y C--------------------------------
+
+        NewQmax = 1
+        for tk in tiempo:# [5min,10min,15min,30min,1h,3h,6h,12h,24h,72h]
+            NewQmax = max(NewQmax, ((0.9*cons["I"][tk]*cons["Aterr"]*(1/1000)) - (0.25*cons["Asot"]/tk))) #aproximadamente
+        NewQmax = int(NewQmax)+2
+
         #-----------------------B1--------------------------------
 
         for tk in tiempo:# [5min,10min,15min,30min,1h,3h,6h,12h,24h,72h]
             h = 1/60 * (tk if tk > 1 else 1)
-            NewQmax = 15.0463125 #aproximadamente
             BQsal = lambda V: Qsal(NewQmax, V)
             resultB = modelar(lambda C,t,newV: (Qent(C,tk)- BQsal(newV)) if t<tk else (0 - BQsal(newV)),
                             lambda V,newC: C(V,newC),
@@ -74,7 +80,7 @@ def main():
                             _euler_Exp)
 
             archivo.write(f"\n\nB1) with tk = {tk}h and h= {h}\n")
-            tabulado = tabulate(resultB,headers=["Tiempo", "Volumen", "C"],tablefmt='grid',stralign='center', numalign= 'center')
+            tabulado = tabulate(resultB,headers=["Tiempo", "Volumen", "C", "H"],tablefmt='grid',stralign='center', numalign= 'center')
             archivo.write(tabulado)
             """print()
             print("B1 with tk = ",tk,"h and h= ",h)
@@ -86,7 +92,6 @@ def main():
         tk = tiempo[4] # 1h
         hrunge = 1/60
         heuler = 1/60
-        NewQmax = 15.0463125 #aproximadamente
         CQsal = lambda V: Qsal(NewQmax, V)
         resultC1Euler = modelar(lambda C,t,newV: (Qent(C,tk)- CQsal(newV)) if t<tk else (0 - CQsal(newV)),
                             lambda V,newC: C(V,newC),
@@ -99,14 +104,14 @@ def main():
 
         archivo.write(f"\n\nC1) with tk = {tk}h\n")
         archivo.write(f"Euler and h= {heuler}\n")
-        tabulado = tabulate(resultC1Euler,headers=["Tiempo", "Volumen", "C"],tablefmt='grid',stralign='center', numalign= 'center')
+        tabulado = tabulate(resultC1Euler,headers=["Tiempo", "Volumen", "C", "H"],tablefmt='grid',stralign='center', numalign= 'center')
         archivo.write(tabulado)
         """print()
         print("C1 with tk = ",tk,"h")
         print("Euler and h= ",heuler)
         print(tabulado)"""
         archivo.write(f"\nRunge and h= {hrunge}\n")
-        tabulado = tabulate(resultC1Runge,headers=["Tiempo", "Volumen", "C"],tablefmt='grid',stralign='center', numalign= 'center')
+        tabulado = tabulate(resultC1Runge,headers=["Tiempo", "Volumen", "C", "H"],tablefmt='grid',stralign='center', numalign= 'center')
         archivo.write(tabulado)
         """print("Runge and h= ",hrunge)
         print(tabulado)"""
